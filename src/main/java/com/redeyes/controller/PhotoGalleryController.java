21d3b3873518764924ffab236208d3bbb1a1949e
Dmytro Briukhatsky <bryukhatsky@gmail.com>
2016-05-24 12:46:00 +0300

    Add uploaded images mapping

--- src/main/java/com/redeyes/controller/PhotoGalleryController.java
+++ src/main/java/com/redeyes/controller/PhotoGalleryController.java
@@ -1,6 +1,10 @@
 package com.redeyes.controller;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
 import com.redeyes.service.PhotoGalleryService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.io.ByteArrayInputStream;
