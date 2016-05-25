818db8182f4c6860f6efbf0615949d891502ac9e
OleksandrDres <oleksandr.dres@gmail.com>
2016-05-25 16:52:32 +0300

    Add javadoc to repository test

--- src/test/java/com/redeyes/repository/PhotoRepositoryImplTest.java
+++ src/test/java/com/redeyes/repository/PhotoRepositoryImplTest.java
@@ -8,9 +8,18 @@ import java.util.Arrays;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+/**
+ * Tests for photo cache.
+ */
 public class PhotoRepositoryImplTest {
+    /**
+     * Photo storage
+     */
     private PhotoRepository photos = new PhotoRepositoryImpl();
 
+    /**
+     * Fill storage mock photos.
+     */
     @Before
     public void before() {
         photos.add(new byte[]{0, 0, 0, 0, 1});
@@ -20,32 +29,50 @@ public class PhotoRepositoryImplTest {
         photos.add(new byte[]{1, 1, 1, 1, 1});
     }
 
+    /**
+     * Add photo to cache test.
+     */
     @Test
     public void testAdd() {
         photos.add(new byte[]{10, 10, 10, 10});
         assertEquals(photos.size(), 6);
     }
 
+    /**
+     * Get photo from
+     */
     @Test
     public void testGet() {
         assertTrue(Arrays.equals(photos.get(3), new byte[]{0, 1, 1, 1, 1}));
     }
 
+    /**
+     * Test cache size.
+     */
     @Test
     public void testSize() {
         assertEquals(photos.size(), 5);
     }
 
+    /**
+     * Init cache test.
+     */
     @Test
     public void testInit() {
         photos.init();
     }
 
+    /**
+     * Get id's test.
+     */
     @Test
     public void testGetIds() {
         assertTrue(photos.getIds().equals(Arrays.asList(0, 1, 2, 3, 4)));
     }
 
+    /**
+     * Get wrong photo test.
+     */
     @Test(expected = IndexOutOfBoundsException.class)
     public void getWrongId() {
         assertTrue(Arrays.equals(photos.get(8), new byte[]{0, 1, 1, 1, 1}));
